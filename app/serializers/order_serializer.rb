d99c8e15f83279640721fa1d9b6d04ba9737b316
Elijah Noble <elijahnobledev@gmail.com>
2022-09-26 16:17:44 -0700

    first commit

---
+++ app/serializers/order_serializer.rb
@@ -0,0 +1,8 @@
+class OrderSerializer < ActiveModel::Serializer
+  attributes :id, :payment_info_id, :shipping_address_id, :payment_verified, :shipment_started
+
+  def show
+        
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  resources :order_products
+  resources :user_products
+  resources :orders
+  resources :products, only: [:index, :show]
+  resources :users
+  resources :payment_infos
+  resources :addresses
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+     
+  # Defines the root path route ("/")
+  namespace :shop do
+    root to: 'shop/products#index'
+  end
 end
+
